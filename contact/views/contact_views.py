8086eeed73f7f98135777847d9764dbd5c0c2704
Rodrigo <rodrigoreis432@gmail.com>
2024-02-29 21:21:50 -0300

    Segundo commit Agenda

---
+++ contact/views/contact_views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from contact.models import Contact
+
+
+def index(request):
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .order_by('-id')[10:20]
+
+    print(contacts.query)
+
+    context = {
+        'contacts': contacts,
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
